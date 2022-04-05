63dd02ef09e8c679b0f80b8894bdd59ee1eb4055
Jorge Alberto Díaz Orozco (Akiel) <diazorozcoj@gmail.com>
2022-04-05 22:22:20 +0200

    Add healthcheck path

--- main.go
+++ main.go
@@ -57,6 +57,10 @@ func main() {
 		}
 	})
 
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("ok"))
+	})
+
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
